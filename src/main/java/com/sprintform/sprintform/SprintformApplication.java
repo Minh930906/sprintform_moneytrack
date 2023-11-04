965d5a2f555921b7ac7ce6600e6f41531b7252d3
Minh930906 <minh930906@gmail.com>
2023-11-04 12:39:46 +0100

    init

---
+++ src/main/java/com/sprintform/sprintform/SprintformApplication.java
@@ -0,0 +1,13 @@
+package com.sprintform.sprintform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sprintform/sprintform/SprintformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprintform.sprintform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
